4346baaf79a85c4b726c5d99764fa29110af90bc
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2023-09-26 09:32:52 -0600

    Fix refresh token spotify api

--- src/scripts/playingNowSpotify.js
+++ src/scripts/playingNowSpotify.js
@@ -1,9 +1,8 @@
 const client_id = 'b98830dfd88f4b03b153bbe48623e883';
 const secret_id = '6e7f3069ac9f485c82fbe77a78b00c19';
-const refresh_token = 'AQBOtkXC9VI9kknN0e2GPv27a-I23LRft16_eRwf8E_uSeb0Q7Pc6TRJma0YpDPvZb9LvMQi64h35-klmckLkF2FUiUFL0inmrt-EYEEpYyNXonjB66XgzuINLtN9zgOxcY'
+const refresh_token = 'AQAReYa6GqbITmLxO_3_L3Lej04jxyXOUE9c7qnb0SeL-RpOKU_9IZV3dE6Jhiag0b7EqnZbjwHGCcAqlypyL6t_gHXVcW3Fgl0tYl3nPPy8bRLdhv_cYpZ9ZK8mJUJBzow'
 
 const tokenEndPoint = 'https://accounts.spotify.com/api/token';
-const nowPlayingEndPoint = 'https://api.spotify.com/v1/me/player/currently-playing';
 const recentlyPlayedEndPoint = 'https://api.spotify.com/v1/me/player/recently-played?limit=1';
 
 const getAccessToken = async () => {
@@ -11,7 +10,7 @@ const getAccessToken = async () => {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
-            Authorization: 'Basic ' + btoa(client_id + ':' + secret_id).toString('base64')
+            Authorization: 'Basic ' + btoa(client_id + ':' + secret_id)
         },
         body: new URLSearchParams({
             grant_type: 'refresh_token',
@@ -37,50 +36,14 @@ const fetchData = async (accessToken, endPoint) => {
     return response.json();
 };
 
-const getNowPlayingItem = async () => {
-    try {
-        const { access_token } = await getAccessToken();
-        const song = await fetchData(access_token, nowPlayingEndPoint);
-        return formatPlayingSong(song);
-    } catch (error) {
-        handleError(error);
-        return null;
-    }
-};
-
 const getRecentlyPlayedItem = async () => {
     try {
         const { access_token } = await getAccessToken();
         const song = await fetchData(access_token, recentlyPlayedEndPoint);
         return formatRecentlySong(song);
     } catch (error) {
-        handleError(error);
         return null;
     }
-};
-
-const formatPlayingSong = (song) => {
-    if (!song) {
-        return null;
-    }
-
-    const albumImageUrl = song.item.album.images[0].url;
-    const artist = song.item.artists.map((_artist) => _artist.name).join(", ");
-    const isPlaying = song.is_playing;
-    const songUrl = song.item.external_urls.spotify;
-    const title = song.item.name;
-    const duration = song.item.duration_ms;
-    const progress = song.progress_ms;
-
-    return {
-        albumImageUrl,
-        artist,
-        isPlaying,
-        songUrl,
-        title,
-        duration,
-        progress,
-    };
 };
 
 const formatRecentlySong = (song) => {
