831e3b9d1a196da771fc91533bfc37f8f7dab7a3
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2023-11-22 18:43:29 -0600

    Fix responsive dark mode card

--- src/components/DarkModeCard.jsx
+++ src/components/DarkModeCard.jsx
@@ -5,8 +5,8 @@ export function DarkModeCard() {
     const { theme, toggleTheme } = useDarkMode()
 
     return (
-        <section className='flex justify-center items-center col-span-1 bg-crayon dark:bg-slate-800 rounded-2xl sm:rounded-3xl aspect-square p-10 gap-4'>
-            <button className='hover:bg-slate-800/10 dark:hover:bg-slate-200/10 rounded-2xl p-4' onClick={toggleTheme}>
+        <section className='flex justify-center items-center col-span-1 bg-crayon dark:bg-slate-800 rounded-2xl sm:rounded-3xl aspect-square gap-4'>
+            <button className='hover:bg-slate-800/10 dark:hover:bg-slate-200/10 rounded-xl p-4' onClick={toggleTheme}>
                 {theme === 'light' ? <SunIcon /> : <MoonIcon />}
             </button>
         </section>
